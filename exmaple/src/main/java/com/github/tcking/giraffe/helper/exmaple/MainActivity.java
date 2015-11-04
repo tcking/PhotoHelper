684500f33adcf4006b076849ed6687ca360d6e97
tom <mytcking@gmail.com>
2015-11-04 11:26:23 +0800

    change activity theme

--- exmaple/src/main/java/com/github/tcking/giraffe/helper/exmaple/MainActivity.java
+++ exmaple/src/main/java/com/github/tcking/giraffe/helper/exmaple/MainActivity.java
@@ -1,10 +1,7 @@
 package com.github.tcking.giraffe.helper.exmaple;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.text.format.Formatter;
 import android.view.View;
@@ -15,7 +12,6 @@ import android.widget.Toast;
 import com.github.tcking.giraffe.helper.PhotoHelper;
 
 import java.io.File;
-import java.util.List;
 
 /**
  * Created by tc(mytcking@gmail.com) on 15/8/20.
@@ -29,11 +25,11 @@ public class MainActivity extends Activity {
 
         //1.get a helper instance
         photoHelper = new PhotoHelper(this,savedInstanceState)
-                .maxWidth(300, true)// image max with 300dp
+                .maxWidth(340, true)// image max with 340dp
                 .autoRotate(true)// autoRotate according to exif information
                 .quality(80)//try compress image using the quality 80
                 .cropping(true)// cropping it after get a photo
-                .maxFileSizeKB(90)//try compress image unless file size < 90KB
+                .maxFileSizeKB(150)//try compress image unless file size < 90KB
                 .callback(new PhotoHelper.CallBack() {
                     @Override
                     public void done(File imageFile) {
@@ -55,23 +51,13 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 if (v.getId() == R.id.btn_choosePhoto) {
                     photoHelper.cropping(false).choosePhoto();
-                }else if (v.getId() == R.id.btn_test) {
-//                    photoHelper.cropping(false).choosePhoto();
-                    WifiManager wifiManager=(WifiManager)getSystemService(Context.WIFI_SERVICE);
-                    wifiManager.startScan();
-                    List<ScanResult> scanResults = wifiManager.getScanResults();
-                    for (int i = 0; i < scanResults.size(); i++) {
-                        ScanResult scanResult = scanResults.get(i);
-                        System.out.println("======"+scanResult.toString());
-                    }
-                } else {
+                }else {
                     photoHelper.takePhoto();
                 }
             }
         };
         findViewById(R.id.btn_choosePhoto).setOnClickListener(clickListener);
         findViewById(R.id.btn_takePhoto).setOnClickListener(clickListener);
-        findViewById(R.id.btn_test).setOnClickListener(clickListener);
     }
 
     @Override
